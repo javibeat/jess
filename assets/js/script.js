23b290c7174492829837e10a443e2909ffd60a8d
javibeat <javibeat@gmail.com>
2023-12-30 14:33:40 +0400

    probando

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var checkbox = document.getElementById('checkbox_toggle');
+    var hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', function() {
+        checkbox.checked = !checkbox.checked; // Esto alternará el estado del checkbox.
+        document.querySelector('.menu').style.display = checkbox.checked ? 'flex' : 'none';
+    });
+});
+
 // Función para filtrar posts según la categoría
 function filterPosts(category) {
     // Selecciona todos los posts
@@ -22,43 +32,6 @@ document.querySelectorAll('.accordion-header').forEach(header => {
         } else {
             document.querySelectorAll('.accordion').forEach(acc => acc.classList.remove('active'));
             accordion.classList.add('active');
-        }
-    });
-});
-
-
-window.addEventListener('scroll', function() {
-    var progress = document.querySelector('.scroll-progress');
-    var containerRect = document.querySelector('.scroll-container').getBoundingClientRect();
-    var containerTop = containerRect.top;
-    var containerHeight = containerRect.height;
-  
-    // La línea roja solo aparecerá y se llenará cuando la sección esté en la vista
-    if(containerTop < window.innerHeight && containerTop >= 0) {
-      var percentage = (window.innerHeight - containerTop) / window.innerHeight;
-      progress.style.height = `${percentage * 100}%`;
-    } else if (containerTop < 0) {
-      progress.style.height = '100%';
-    } else {
-      progress.style.height = '0%';
-    }
-  });
-  
-  document.addEventListener('DOMContentLoaded', (event) => {
-    // Obtener el botón y el menú desplegable
-    var dropbtn = document.querySelector('.dropbtn');
-    var dropdown = document.querySelector('.dropdown');
-
-    // Añadir evento click al botón
-    dropbtn.addEventListener('click', function() {
-        // Mostrar u ocultar el menú desplegable
-        dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
-    });
-
-    // Cerrar el menú desplegable si se hace clic fuera de él
-    window.addEventListener('click', function(e) {
-        if (!dropbtn.contains(e.target)) {
-            dropdown.style.display = 'none';
         }
     });
 });
